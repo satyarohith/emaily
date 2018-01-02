aa4d251763fc5e808c87b7036ff751c4314b43fc
Satya Rohith <gsatyarohith@gmail.com>
2018-01-02 20:11:31 +0530

    Connected mlab and created models and reconfigured file structure

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 const express = require('express'),
   app = express(),
   port = process.env.PORT || 5000,
-  passport = require('passport'),
-  GoogleStrategy = require('passport-google-oauth20').Strategy,
-  Keys = require('./config/keys');
+  mongoose = require('mongoose'),
+  Keys = require('./config/Keys');
+require('./services/passport');
 
+mongoose.connect(Keys.mongoURI);
 // google keys
 /*
 * client ID: 786186387466-onsm9mpg3d923egcpri0cq4cnu2k8id0.apps.googleusercontent.com 
@@ -12,34 +13,10 @@ const express = require('express'),
 */
 
 // passport stuff
-passport.use(
-  new GoogleStrategy(
-    {
-      clientID: Keys.googleClientID,
-      clientSecret: Keys.googleClientSecret,
-      callbackURL: '/a/g/cb'
-    },
-    (accessToken, refreshToken, profile, done) => {
-      console.log('access Token:', accessToken);
-      console.log('refresh Token:', refreshToken);
-      console.log('Profile :', profile);
-    }
-  )
-);
 
 // Routes
-app.get('/', (req, res) => {
-  res.send('Hello There');
-});
+require('./routes/authRoutes')(app);
 
-app.get(
-  '/a/g', // url
-  passport.authenticate('google', {
-    scope: ['profile', 'email']
-  })
-);
-
-app.get('/a/g/cb', passport.authenticate('google'));
 // App Listens here
 app.listen(port, () => {
   console.log(`server running at localhost:${port}`);
