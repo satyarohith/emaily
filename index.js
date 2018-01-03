447c95aa5b997ac895841f4b0af5fa52c6c146d4
Satya Rohith <gsatyarohith@gmail.com>
2018-01-03 20:10:23 +0530

    fixed config file and heroku deployment

--- index.js
+++ index.js
@@ -9,11 +9,6 @@ require('./models/User');
 require('./services/passport');
 
 mongoose.connect(Keys.mongoURI);
-// google keys
-/*
-* client ID: 786186387466-onsm9mpg3d923egcpri0cq4cnu2k8id0.apps.googleusercontent.com 
-* Client Secret: AYm3mkA1SOWX8QzhDIUfjVXY 
-*/
 
 // passport stuff
 
