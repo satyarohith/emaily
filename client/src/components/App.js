22fa4191acb476ff93977428cb81a39c15903233
Satya Rohith <gsatyarohith@gmail.com>
2018-01-08 23:01:27 +0530

    LIP: react routers and reducers setup

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,9 +1,22 @@
 import React from 'react';
+import { BrowserRouter, Route } from 'react-router-dom';
+
+const Header = () => <h2>Header</h2>;
+const Dashboard = () => <h2>Dashboard</h2>;
+const SurveyNew = () => <h2>SurveyNew</h2>;
+const Landing = () => <h2>landing</h2>;
 
 const App = () => {
   return (
     <div>
-      <h1>Hi I'm alive!</h1>
+      <BrowserRouter>
+        <div>
+          <Header />
+          <Route exact path="/" component={Landing} />
+          <Route path="/surveys" component={Dashboard} />
+          <Route exact path="/Surveys/New" component={SurveyNew} />
+        </div>
+      </BrowserRouter>
     </div>
   );
 };
